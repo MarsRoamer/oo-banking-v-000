11dffe359b1eac6e6876dc6a7818f1e090eff057
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-12 18:02:24 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ require 'pry'
 
   def execute_transaction
 
-    if !@sender.valid?
+    if !@sender.BankAccount.valid?
       "Transaction rejected. Please check your account balance."
 
     else
